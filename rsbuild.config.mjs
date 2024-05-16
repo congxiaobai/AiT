ab23e5aa1c3a45c36c9b1644036b9a68eda78022
wangshuang40 <wangshuang40@meituan.com>
2024-05-16 11:07:17 +0800

    一次性的打包配置

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -1,29 +1,38 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 import { pluginSvgr } from '@rsbuild/plugin-svgr';
+import path from 'node:path';
 
 export default defineConfig({
   plugins: [pluginReact(), pluginSvgr()],
-  html: {
-    template: './public/index.html',
-  },
   source: {
     entry: {
       index: './src/index.jsx',
       setting: './src/setting/index.jsx',
-      background:'./src/background.jsx'
-    }  
+      background: './src/background.js',
+    } ,
   },
-
+  html: {
+    template({ entryName }) {
+      const templates = {
+        index: './public/index.html',
+        setting: './public/setting.html',
+      }
+      return templates[entryName] 
+    }
+  },
+  tools: {
+    htmlPlugin: false,
+  },
   output: {
     filenameHash: false,
     legalComments: 'none',
     distPath: {
       js: './', css: './'
     },
-    sourceMap: {
-      js: 'source-map',
-    },
+    // sourceMap: {
+    //   js: 'source-map',
+    // },
     copy: [{ from: './manifest.json', to: 'manifest.json' },
 
     { from: './src/content.js', to: 'content.js' },
